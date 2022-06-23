b71d3ebb5ddf9a38c42fea38a8c480bd9bed8ffb
ryoji-ishii <goccha.dev@gmail.com>
2022-06-23 17:52:21 +0900

    Support for arrays

--- masking/masking_test.go
+++ masking/masking_test.go
@@ -16,9 +16,11 @@ func TestProcessor_Mask(t *testing.T) {
 		Address string `json:"address"`
 	}
 	type Body struct {
-		UserId   string `json:"userId"`
-		Password string `json:"password"`
-		Profile  `json:"profile"`
+		UserId      string `json:"userId"`
+		Password    string `json:"password"`
+		Profile     `json:"profile"`
+		Families    []Profile `json:"profiles"`
+		Introducers []string  `json:"introducers"`
 	}
 
 	body := &Body{
@@ -31,13 +33,30 @@ func TestProcessor_Mask(t *testing.T) {
 			Phone:   "00-0123-4567",
 			Address: "東京都",
 		},
+		Families: []Profile{
+			{
+				Name:    "山田 花子",
+				Age:     30,
+				Gender:  "女",
+				Phone:   "00-0123-4568",
+				Address: "東京都",
+			},
+			{
+				Name:    "山田 一",
+				Age:     3,
+				Gender:  "男",
+				Phone:   "",
+				Address: "東京都",
+			},
+		},
+		Introducers: []string{"田中 太郎", "鈴木 一郎"},
 	}
 	ctx := context.Background()
 
 	data, err := json.Marshal(body)
 	assert.NoError(t, err)
 
-	data, err = New("password", "name", "age", "phone", "address").Run(ctx, data)
+	data, err = New("password", "name", "age", "phone", "address", "introducers").Run(ctx, data)
 	assert.NoError(t, err)
 
 	err = json.Unmarshal(data, body)
@@ -51,4 +70,20 @@ func TestProcessor_Mask(t *testing.T) {
 	assert.Equal(t, "男", body.Gender)
 	assert.Equal(t, "test_user", body.UserId)
 
+	for i, v := range body.Families {
+		if i == 0 {
+			assert.Equal(t, MaskValue, v.Name)
+			assert.Equal(t, MaskNumber, v.Age)
+			assert.Equal(t, MaskValue, v.Phone)
+			assert.Equal(t, MaskValue, v.Address)
+		} else {
+			assert.Equal(t, MaskValue, v.Name)
+			assert.Equal(t, MaskNumber, v.Age)
+			assert.Equal(t, "", v.Phone)
+			assert.Equal(t, MaskValue, v.Address)
+		}
+	}
+	for _, v := range body.Introducers {
+		assert.Equal(t, MaskValue, v)
+	}
 }
