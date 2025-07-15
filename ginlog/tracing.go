574677b2ea444a63bc072d09d82742e82e0cf4ad
ryoji-ishii <goccha.dev@gmail.com>
2025-07-15 19:58:27 +0900

    enhance tracing span attributes for improved HTTP request logging

--- ginlog/tracing.go
+++ ginlog/tracing.go
@@ -2,6 +2,7 @@ package ginlog
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/goccha/http-constants/pkg/headers"
 	"github.com/goccha/logging/log"
 	"github.com/goccha/logging/tracing"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
@@ -25,11 +26,27 @@ func TraceRequest(tracer trace.Tracer, dump bool, f tracing.NewFunc) gin.Handler
 		}
 		ctx := c.Request.Context()
 		span := trace.SpanFromContext(ctx)
+		span.SetAttributes(semconv.HTTPScheme(c.Request.URL.Scheme))
+		span.SetAttributes(semconv.HTTPMethod(c.Request.Method))
+		span.SetAttributes(semconv.HTTPTarget(c.Request.URL.Path))
 		span.SetAttributes(semconv.HTTPURL(c.Request.URL.String()))
+		if l := c.Request.ContentLength; l > 0 {
+			span.SetAttributes(semconv.HTTPRequestContentLength(int(l)))
+		}
+		if ip := tracing.ClientIP(c.Request); ip != "" {
+			span.SetAttributes(semconv.HTTPClientIP(ip))
+		}
+		if ua := c.Request.Header.Get(headers.UserAgent); ua != "" {
+			span.SetAttributes(semconv.HTTPUserAgent(ua))
+		}
 		c.Request = c.Request.WithContext(tracing.With(ctx, c.Request, f))
 		if dump {
-			log.Dump(c.Request.Context(), log.Debug(c.Request.Context())).Msg("dump")
+			log.Dump(ctx, log.Debug(ctx)).Msg("dump")
 		}
 		c.Next()
+		span.SetAttributes(semconv.HTTPStatusCode(c.Writer.Status()))
+		if c.Writer.Size() > 0 {
+			span.SetAttributes(semconv.HTTPResponseContentLength(c.Writer.Size()))
+		}
 	}
 }
